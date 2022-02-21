815d14eacff4a46371761eed66a9fdec7868a48e
JpfSource <91543129+JpfSource@users.noreply.github.com>
2022-02-21 17:31:50 +0100

    Personne + identité ok

--- src/app/components/person/person-identity/person-identity.component.ts
+++ src/app/components/person/person-identity/person-identity.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
 import { Person } from 'src/app/shared/interfaces/person';
 import { PersonService } from 'src/app/shared/services/person.service';
 import { Location } from '@angular/common';
@@ -15,11 +14,6 @@ import { Location } from '@angular/common';
 export class PersonIdentityComponent implements OnInit {
 
   identityForm!: FormGroup;
-
-  // public person$ = new BehaviorSubject<Person | null>(null);
-
-  id!: number;
-
   person!: Person | null;
 
   constructor(
@@ -32,61 +26,6 @@ export class PersonIdentityComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // this.person$ = this._personService.person$;
-    //vérfier si la vakeur de person$ est null dans ce cas récuperer avec méthode getById par l'url
-    if(this._personService.person$.getValue()?.id == null) {
-      this._route.paramMap.subscribe(param => {
-
-        const personId = Number(param.get('id'));
-
-        if(personId != null && personId > 0) {
-          console.log(personId);
-          // this._personService.getPersonById(personId).subscribe(data => this.person$.next(data));
-          this._personService.getPersonById(personId);
-        }
-
-
-      })
-    }
-    // this._personService.getPersonById(1);
-
-    this._personService.person$.subscribe((u: Person | null) => {
-      console.log(u);
-
-      this.person = u;
-
-      this.identityForm.controls['nom'].setValue(this.person?.nom);
-      this.identityForm.controls['prenom'].setValue(this.person?.prenom);
-      this.identityForm.controls['description'].setValue(this.person?.description);
-      this.identityForm.controls['dateNaissance'].setValue(this.person?.dateNaissance);
-      this.identityForm.controls['id'].setValue(this.person?.id);
-    });
-    this.initForm();
-  }
-  /**
-   * Méthode qui permet de donner des valeurs par défaut en fonction de la BdD
-   */
-  initForm() {
-    // this._route.paramMap.subscribe(param => {
-    //   const personId = Number(param.get('id'));
-    //   this.id=personId;
-    // })
-
-    // this._personService.getPersonById(this.id).subscribe(data => {
-    //   // this.person$.next(data);
-    //   this.person = data;
-    //  this.identityForm.controls['nom'].setValue(this.person.nom);
-    //   this.identityForm.controls['prenom'].setValue(this.person.prenom);
-    //   this.identityForm.controls['description'].setValue(this.person.description);
-    //   this.identityForm.controls['dateNaissance'].setValue(this.person.dateNaissance);
-    //   this.identityForm.controls['id'].setValue(this.id);
-
-    //   console.log(this.person.nom);
-    //   console.log(this.person.urlPhoto);
-
-    //   // this.person$.next(data);
-    // });
-
     this.identityForm = this._fb.group({
       nom: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
       prenom: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
@@ -94,33 +33,27 @@ export class PersonIdentityComponent implements OnInit {
       dateNaissance: ['', Validators.required],
       id: ['']
     });
+
+    this._personService.person$.subscribe((p: Person) => {
+      this.person = p;
+      this.identityForm.controls['nom'].setValue(this.person.nom);
+      this.identityForm.controls['prenom'].setValue(this.person?.prenom);
+      this.identityForm.controls['description'].setValue(this.person?.description);
+      this.identityForm.controls['dateNaissance'].setValue(this.person?.dateNaissance);
+    });
+
   }
 
   submitForm() {
-    /**
-     * vérifier validité du form,
-     * faire un patch/put en bdd -> méthoe à créer dans le service
-     * person.service
-     */
-    console.log(this.identityForm.value);
-    const p = { ...this.person, ...this.identityForm.value };
-    console.log(p);
-
     if (this.identityForm.valid) {
-
-      // this._route.paramMap.subscribe(param => {
-      //   const personId = Number(param.get('id'));
+      this.identityForm.value.id = this.person?.id;
+      const p = { ...this.person, ...this.identityForm.value };
       this._personService.updateProfil(p);
-
-      // })
+      this.goBack();
     }
-
   }
 
-  //faire un navigate avec le router
   goBack(): void {
-    this.location.back();
+    this._router.navigateByUrl("person/"+ this.person?.id);
   }
-
-  //supprimer l'abonnement dans OnDestroy
 }
