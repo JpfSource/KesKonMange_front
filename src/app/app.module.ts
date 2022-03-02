8b2697fb81a10040670e7e913c96e1aa00719cec
INGOLD <fogold07@gmail.com>
2022-03-02 12:47:46 +0100

    suppression components non utilisés

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,12 @@ import { AuthComponent } from './components/auth/auth.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
-import { MenuTabsComponent } from './components/menu-tabs/menu-tabs.component';
 import { PersonIdentityComponent } from './components/person/person-identity/person-identity.component';
 import { PersonMorphologyComponent } from './components/person/person-morphology/person-morphology.component';
 import { PersonComponent } from './components/person/person.component';
 import { ItemPlatComponent } from './components/plats/item-plat/item-plat.component';
 import { PersonPlatsComponent } from './components/plats/person-plats/person-plats.component';
 import { BoardUserComponent } from './components/user/board-user/board-user.component';
-import { ProfileComponent } from './components/user/profile/profile.component';
 import { authInterceptorProviders } from './shared/helpers/auth.interceptor';
 
 
@@ -28,9 +26,7 @@ import { authInterceptorProviders } from './shared/helpers/auth.interceptor';
     PersonComponent,
     PersonMorphologyComponent,
     PersonIdentityComponent,
-    MenuTabsComponent,
     HomeComponent,
-    ProfileComponent,
     BoardUserComponent,
     AuthComponent,
     PersonPlatsComponent,
