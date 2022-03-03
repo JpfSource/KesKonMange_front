24f3dda16202d226f1cfa08609964004732bf4ba
INGOLD <fogold07@gmail.com>
2022-03-03 23:46:14 +0100

    recherche mots-clés OK, filtrer par type de plat OK

--- src/app/components/plats/person-plats/person-plats.component.ts
+++ src/app/components/plats/person-plats/person-plats.component.ts
@@ -14,20 +14,16 @@ import { PlatService } from 'src/app/shared/services/plat.service';
 })
 export class PersonPlatsComponent implements OnInit, AfterViewInit {
 
-  filtre!: string;
-  person!: Person | null;
-  plat!: Plat;
-  public plats$ = new BehaviorSubject<Plat[]>([]);
-  platId!: number;
-  message!: string;
-  filtrePlat!: string;
 
-  //POUR LA RECHERCHE PAR MOTS CLES
-  //******************
+  person!: Person | null;
+  plats$ = new BehaviorSubject<Plat[]>([]);
+  message!: string;
+  typePlatInputSaved!: string;
+  mesPlatsFiltres$ = new BehaviorSubject<Plat[]>([]);
+
   @ViewChild(CriteriaComponent)
   filterComponent!: CriteriaComponent;
   parentListFilter!: string;
-  //******************
 
   typePlats: string[] = ["Entrée", "Plat principal", "Dessert", "Laitage", "Boisson", "Céréales"]
 
@@ -38,57 +34,51 @@ export class PersonPlatsComponent implements OnInit, AfterViewInit {
     private _router: Router
   ) { }
 
-  //******************
   ngAfterViewInit(): void {
     this.parentListFilter = this.filterComponent.listFilter;
   }
-  //******************
+
   private typeSelectedSubject = new BehaviorSubject<string>("");
   typeSelectedAction$ = this.typeSelectedSubject.asObservable();
 
-  // mesPlatsFiltres$ = new BehaviorSubject<Plat[]>([]);
-  mesPlatsFiltres$ = combineLatest([
-    this.plats$,
-    this.typeSelectedAction$
-  ])
-    .pipe(
-      map(([plats, selectedType]) =>
-        plats.filter(plat => selectedType ? plat.typePlatLibelle === selectedType : true
-        ))
-    );
-
   onSelected(typePlatInput: string) {
-    this.typeSelectedSubject.next(typePlatInput);
+    this.typePlatInputSaved = typePlatInput;
+    this.mesPlatsFiltres$.next(this.plats$.value);
+    if(typePlatInput != "") {
+      this.mesPlatsFiltres$.next(
+        this.mesPlatsFiltres$.value
+        .filter(plat =>
+          plat.typePlatLibelle == typePlatInput!
+          ))
+    }
   }
 
   getAllPlats(): void {
     this._platService.findAll().subscribe(plats => {
       this.plats$.next(plats);
-      this.performFilter(this.parentListFilter);
+      this.mesPlatsFiltres$.next(plats);
+      // this.performFilter(this.parentListFilter);
     });
   }
 
-  //******************
   onValueChange(value: string): void {
     this.performFilter(value);
   }
 
-  /** NE MARCHE PAS ENCORE N'ENTRE PAS DANS LE PIPE
-  */
   performFilter(filter?: string): void {
-    console.log("event list");
-
-    this.mesPlatsFiltres$.pipe(
-        tap(() => console.log("dans pipe")),
-        map(plats =>
-          plats.filter(plat => plat.libelle?.toLocaleLowerCase().includes(filter!.toLocaleLowerCase()))
-            // .indexOf(filter!.toLocaleLowerCase()) !== -1)
-        ),
-        tap(plats => console.log(plats)))
-
+    if(filter){
+      this.mesPlatsFiltres$.next(
+        this.mesPlatsFiltres$.value
+        .filter(plat =>
+          plat.libelle?.toLocaleLowerCase()
+          .includes(filter!.toLocaleLowerCase()))
+          )
+    }
+    else {
+      this.mesPlatsFiltres$.next(this.plats$.value);
+      this.onSelected(this.typePlatInputSaved);
+    }
   }
-  //******************
-
 
   ngOnInit(): void {
     this._route.paramMap.subscribe(param => {
@@ -100,24 +90,13 @@ export class PersonPlatsComponent implements OnInit, AfterViewInit {
         });
       }
     });
-
     this.getAllPlats();
-
   }
 
   public deletePlat(id: any): void {
     this._platService.deletePlat(id).subscribe(() => {
       this.message = "Plat supprimé avec succès !"
       this.getAllPlats();
-      this.mesPlatsFiltres$ = combineLatest([
-        this.plats$,
-        this.typeSelectedAction$
-      ])
-        .pipe(
-          map(([plats, selectedType]) =>
-            plats.filter(plat => selectedType ? plat.typePlatLibelle === selectedType : true
-            ))
-        );
       setTimeout(() => this.message = "", 2500);
     });
 
